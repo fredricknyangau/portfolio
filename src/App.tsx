70ad5d338744b3f5cc8b97af8efd41fa7702e29f
Fredrick Nyangau (Zeal) <zeal@fredricknyangau.com>
2026-04-16 10:13:28 +0300

    feat: integrate Vercel Analytics into the application root

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from '@/pages/Layout';
 import Home from '@/pages/Home';
 import CaseStudy from '@/pages/CaseStudy';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App(): JSX.Element {
   return (
+    <div>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="case-study/:id" element={<CaseStudy />} />
       </Route>
     </Routes>
+    <Analytics />
+    </div>
   );
 }
